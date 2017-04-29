487b15b3b2f35838b663dca4d38fae43f4e2781f
Tim Starmer <tim.starmer@gmail.com>
2017-04-28 20:13:24 -0400

    crude onchange handler added to newpoll form

--- src/components/NewPoll.js
+++ src/components/NewPoll.js
@@ -2,7 +2,12 @@ import React, { Component } from 'react';
 
 class NewPoll extends Component{
 	constructor(props){
-		super(props)
+		super(props);
+		this.state={
+			title: "Poll Title",
+			description: "Poll description",
+			choices:"choices...."
+		}
 	}
 // value={} onChange={}
 	parseSubmit = (e) =>{
@@ -12,6 +17,12 @@ class NewPoll extends Component{
 
 	}
 
+	onChange = (e) =>{
+		console.log(e.target.value)
+		this.setState({
+			[e.target.name]:e.target.value
+		})
+	}
 
 	render(){
 		return(
@@ -19,11 +30,11 @@ class NewPoll extends Component{
 				
 				<form onSubmit={this.parseSubmit}>	
 					<h4>Poll Title</h4>
-					<input type="text" className="title"></input>
+					<input type="text" className="title" name="title" onChange={this.onChange} value={this.state.title}></input>
 					<h6>Poll Description</h6>
-					<input type="text" className="title"></input>
+					<input type="text" className="description-text" name="description"onChange={this.onChange} value={this.state.description}></input>
 					<h6>Poll Choices</h6>
-					<input type="text" className="title"></input>
+					<input type="text" className="choices-text" name="choices" onChange={this.onChange} value={this.state.choices}></input>
 					<input type="submit" className="btn"></input>
 					<button onClick={this.props.onClose}>Close</button>
 				</form>
